3db72dd7945074e55aa90768751c29afce2fc18d
jrichelsen <jrichelsen@gmail.com>
2015-04-30 13:45:56 -0400

    Fix paddle going off screen

--- Paddle.py
+++ Paddle.py
@@ -29,23 +29,35 @@ class Paddle(pygame.sprite.Sprite):
 		self.is_moving_down = False
 
 	def tick(self):
-		if self.is_moving_left and not self.is_out_left_bound():
-			self.rect.move_ip(-PADDLE_SPEED, 0)
-		if self.is_moving_right and not self.is_out_right_bound():
-			self.rect.move_ip(PADDLE_SPEED, 0)
-		if self.is_moving_up and not self.is_out_top_bound():
-			self.rect.move_ip(0, -PADDLE_SPEED)
-		if self.is_moving_down and not self.is_out_bottom_bound():
-			self.rect.move_ip(0, PADDLE_SPEED)
+		if self.is_moving_left:
+				if self.is_out_left_bound(self.rect.move(-PADDLE_SPEED, 0)):
+					self.rect.left = 0
+				else:
+					self.rect.move_ip(-PADDLE_SPEED, 0)
+		if self.is_moving_right:
+				if self.is_out_right_bound(self.rect.move(PADDLE_SPEED, 0)):
+					self.rect.right = SCREEN_WIDTH
+				else:
+					self.rect.move_ip(PADDLE_SPEED, 0)
+		if self.is_moving_up:
+				if self.is_out_top_bound(self.rect.move(0, -PADDLE_SPEED)):
+					self.rect.top = 0
+				else:
+					self.rect.move_ip(0, -PADDLE_SPEED)
+		if self.is_moving_down:
+				if self.is_out_bottom_bound(self.rect.move(0, PADDLE_SPEED)):
+					self.rect.bottom = SCREEN_HEIGHT
+				else:
+					self.rect.move_ip(0, PADDLE_SPEED)
 
-	def is_out_left_bound(self):
-		return self.rect.left < 0
+	def is_out_left_bound(self, rect):
+		return rect.left < 0
 
-	def is_out_right_bound(self):
-		return self.rect.right > SCREEN_WIDTH
+	def is_out_right_bound(self, rect):
+		return rect.right > SCREEN_WIDTH
 
-	def is_out_top_bound(self):
-		return self.rect.top < 0
+	def is_out_top_bound(self, rect):
+		return rect.top < 0
 
-	def is_out_bottom_bound(self):
-		return self.rect.bottom > SCREEN_HEIGHT	  
+	def is_out_bottom_bound(self, rect):
+		return rect.bottom > SCREEN_HEIGHT	  
